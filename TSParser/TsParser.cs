5933fd78077a7c36b1fe002cd36285aad1714f9a
deimspb <deim.mobile@gmail.com>
2021-12-12 20:10:33 +0300

    add public methods

--- TSParser/TsParser.cs
+++ TSParser/TsParser.cs
@@ -3,19 +3,91 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TSParser.Descriptors;
+using TSParser.Enums;
+using TSParser.Tables;
+using TSParser.TransportStream;
 
 namespace TSParser
 {
     public class TsParser
     {
         #region Private fields
+        private delegate void m_currentTableFactory(TsPacket tsPacket);
 
         #endregion
         #region Public methods
+        public TsParser(TsMode mode = TsMode.DVB)
+        {
 
+        }
+        public TsParser(string tsFile, TsMode mode = TsMode.DVB)
+        {
+
+        }
+        public TsParser(string mcastAddress,int port, string mcastInterface,TsMode mode = TsMode.DVB)
+        {
+
+        }
+        public void RunParser()
+        {
+
+        }
+        public void RunParser(int milliseconds)
+        {
+
+        }
+        public async Task RunParserAsync()
+        {
+
+        }
+        public async Task RunParserAsync(int milliseconds)
+        {
+
+        }
+        public void StopParser()
+        {
+
+        }
+        //parse bytes from source
+        public void PushBytes()
+        {
+
+        }
+        public TsPacket[] GetTsPacketsFromBytes(ReadOnlySpan<byte> bytes)
+        {
+
+        }
+        public TsPacket GetOneTsPacketFromBytes(ReadOnlySpan<byte> bytes)
+        {
+
+        }
+        public Table GetOneTableFromBytes(ReadOnlySpan<byte> bytes)
+        {
+
+        }
+        public Descriptor GetOnedescriptorFromBytes(ReadOnlySpan<byte> bytes)
+        {
+
+        }
         #endregion
         #region Private methods
+        private void InitEvents()
+        {
 
+        }
+        private void DvbTableFactory(TsPacket tsPacket)
+        {
+
+        }
+        private void AtscTableFactory(TsPacket tsPacket)
+        {
+
+        }
+        private void IsdbTableFactory(TsPacket tsPacket)
+        {
+
+        }
         #endregion
     }
 }
