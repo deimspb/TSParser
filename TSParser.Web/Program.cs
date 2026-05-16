64b044e40c45abecf4fe28459b8e6bbd40dcf42c
MATRIX Deim <deim.mobile@gmail.com>
2026-05-17 01:22:36 +0300

    step 5

---
+++ TSParser.Web/Program.cs
@@ -0,0 +1,24 @@
+// Run from repo root: dotnet run --project TSParser.Web
+using TSParser.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
